2c6145f8b4a213e9909a6019b3dfeb4e9729195a
Meaookung144 <kittinanatthasiri@gmail.com>
2025-07-09 12:31:47 +0700

    ...

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
+<<<<<<< HEAD:postcss.config.js
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
+=======
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
+>>>>>>> parent of c5116db (again):postcss.config.mjs
